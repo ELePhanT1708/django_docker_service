272d8b2ca4b4397f711c8343b95cfb6af1681324
ELePhanT1708 <elephant1708@gmail.com>
2023-01-05 14:02:57 +0300

    Day 3 - lesson 4 --- Created annotate fields and aggregate fields in request body using python calculations and db calculations separately

--- service/services/views.py
+++ service/services/views.py
@@ -1,5 +1,4 @@
-from django.db.models import Prefetch
-from django.shortcuts import render
+from django.db.models import Prefetch, Sum, F
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -10,7 +9,17 @@ from services.serializers import SubscriptionSerializer
 # Create your views here.
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
+        'service',
         Prefetch('client',
                  queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    )
+    ).annotate(price=F('service__full_price') * (100 - F('plan__discount_percent'))/100)
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+        response_data = {'result': response.data, 'total': queryset.aggregate(total=Sum('price')).get('total')}
+        response.data = response_data
+        return response
+
